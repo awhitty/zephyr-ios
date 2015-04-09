efb77bddc724e034b98cf620c3f9dbddccaab1fb
Austin Whittier <austin.whitt@gmail.com>
2015-04-09 16:45:33 -0700

    pods - add obj-c bridging file with ionicons import

---
+++ Zephyr One/Zephyr-One-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Zephyr-One-Bridging-Header.h
+//  Zephyr One
+//
+//  Created by Austin Whittier on 4/9/15.
+//  Copyright (c) 2015 Zephyr Racing. All rights reserved.
+//
+
+#ifndef Zephyr_One_Zephyr_One_Bridging_Header_h
+#define Zephyr_One_Zephyr_One_Bridging_Header_h
+
+#import "IonIcons.h"
+
+#endif
