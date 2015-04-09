7296c33433ff88b969c1cbd82484bb4ec0488836
Austin Whittier <austin.whitt@gmail.com>
2015-04-09 16:45:56 -0700

    interface - associate view controllers with feed and drive views

---
+++ Zephyr One/DriveViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DriveViewController.swift
+//  Zephyr One
+//
+//  Created by Austin Whittier on 4/9/15.
+//  Copyright (c) 2015 Zephyr Racing. All rights reserved.
+//
+
+import UIKit
+
+class DriveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
