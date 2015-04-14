72c4ff9fb44de42864199d485cc768f84f488e70
Miraj Rahematpura <mirajr@stanford.edu>
2015-04-14 11:15:31 -0700

    stopwatch calculations

--- Zephyr One/DriveViewController.swift
+++ Zephyr One/DriveViewController.swift
@@ -29,13 +29,53 @@ class DriveViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-//    @IBOutlet weak var stopWatchDisplay: UILabel!
-//
-//    @IBAction func startStopWatch(sender: AnyObject) {
-//    }
-//    
-//    @IBAction func stopStopWatch(sender: AnyObject) {
-//    }
+    @IBOutlet weak var curTrackTitle: UILabel!
+    @IBOutlet weak var curSpeed: UILabel!
+    @IBOutlet weak var lastLapTime: UILabel!
+    @IBOutlet weak var SWDisplay: UILabel!
+    
+    var startTime = NSTimeInterval()
+  
+    func updateTime() {
+       
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: NSTimeInterval = (currentTime - startTime)
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        let strMinutes = minutes > 9 ? String(minutes):"0" + String(minutes)
+        let strSeconds = seconds > 9 ? String(seconds):"0" + String(seconds)
+        let strFraction = fraction > 9 ? String(fraction):"0" + String(fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        SWDisplay.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
+    
+    var timer = NSTimer()
+    @IBAction func startSW(sender: AnyObject) {
+        if !timer.valid {
+            let aSelector : Selector = "updateTime"
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
+    }
+    
+    @IBAction func stopSW(sender: AnyObject) {
+        timer.invalidate()
+//        timer == nil
+    }
     
     /*
     // MARK: - Navigation
