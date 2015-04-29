687d02ca453c2279892e25db4553582095bcf8f0
Miraj Rahematpura <mirajr@stanford.edu>
2015-04-29 03:56:47 -0700

    more login -- got login through Facebook to work and booleans to track if current user is signed in--> need name and photo

--- Zephyr One/MoreLoginController.swift
+++ Zephyr One/MoreLoginController.swift
@@ -22,24 +22,51 @@ class MoreLoginController: UIViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
+    var isSignedIn = true;
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        let currentUser = PFUser.currentUser()
-        
+        var currentUser = PFUser.currentUser()
         
         if currentUser != nil {
             self.username.text = currentUser?.username //need to get user first name and last name
             self.signInButton.setTitle("Sign out", forState: UIControlState.Normal)
+        
         } else {
-            self.username.text = "not signed in"
+            self.username.text = "Please Sign In"
             self.signInButton.setTitle("Sign in", forState: UIControlState.Normal)
+            isSignedIn = false;
         }
     }
     
     @IBAction func signInPressed(sender: UIButton) {
 //        sender.setTitle("Sign out", forState: UIControlState.Normal)
-        
-        println("We would sign in here")
+        var currentUser = PFUser.currentUser()
+        if (!isSignedIn) {
+            PFFacebookUtils.logInInBackgroundWithReadPermissions(["public_profile", "email", "user_friends"], block: {
+                (user: PFUser?, error: NSError?) -> Void in
+                if let user = user {
+                    if user.isNew {
+                        println("User signed up and logged in through Facebook!")
+                    } else {
+                        println("User logged in through Facebook!")
+                    }
+                    currentUser = PFUser.currentUser()
+                    self.username.text = currentUser?.username //need to get user first name and last name
+                    self.signInButton.setTitle("Sign out", forState: UIControlState.Normal)
+                    self.isSignedIn = true;
+                } else {
+                    println("Uh oh. The user cancelled the Facebook login.")
+                }
+            })
+        } else {
+            PFUser.logOut()
+            var currentUser = PFUser.currentUser()
+            self.username.text = "Please Sign In"
+            self.signInButton.setTitle("Sign in", forState: UIControlState.Normal)
+            isSignedIn = false;
+
+        }
     }
 }
