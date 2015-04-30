7e2b92aa9707ef808e75ef24052a0f2835b65a04
Miraj Rahematpura <mirajr@stanford.edu>
2015-04-30 08:27:44 -0700

    settings -- adding car details page

---
+++ Zephyr One/SettingsCarSetupController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsCarSetup.swift
+//  Zephyr One
+//
+//  Created by Owner on 4/30/15.
+//  Copyright (c) 2015 Zephyr Racing. All rights reserved.
+//
+
+import Foundation
+
+class SettingsCarSetupController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        //         self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+    }
+    
+
+}
